17e087bbc97c7e7a79fee18ad22c94cc74c8cf33
Atharv varshney <Atharvvarshney07@gmail.com>
2024-04-22 17:27:35 +0530

    Fixes

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,6 +1,5 @@
 import cloudinary from "cloudinary";
 import { NextResponse } from "next/server";
-import bcrypt from "bcrypt";
 
 cloudinary.v2.config({
     cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
@@ -9,34 +8,13 @@ cloudinary.v2.config({
     secure: true,
 });
 
-
-
 export async function GET(req, res) {
-
-    // const apiUrl = new URL(req.nextUrl)
-    // const secretKey = apiUrl.searchParams.get("secret")
-
-    // if (secretKey) {
-    //     const hash = await bcrypt.hash("digex_super_secret_password", 2);
-    //     const isCorrect = await bcrypt.compare(secretKey, hash);
-    //     if (!isCorrect) {
-    //         return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
-    //     }
-    // }
-    // else {
-    //     return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
-    // }
-
-    if (!process.env.NEXT_PUBLIC_CLOUDINARY_FOLDER) {
-        return NextResponse.json({ message: 'Error fetching ENV' }, { status: 500 });
-    }
     try {
         const results = await cloudinary.v2.search
             .expression(`folder:${process.env.NEXT_PUBLIC_CLOUDINARY_FOLDER}/*`)
             .sort_by("public_id", "desc")
             .max_results(10)
             .execute();
-        // const results = []
         return NextResponse.json(results, { status: 200 });
     } catch (error) {
         console.error(error);

457a3fd2ad381c398f973a8387465a105209a85c
aludayalu <pl46u3.ducklinz@gmail.com>
2024-05-02 23:27:10 +0530

    added infinite scrolling

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,17 @@
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { Masonry } from 'react-plock';
+import { useRef } from 'react';
+import { useIntersection } from '@mantine/hooks';
 
 export default function Gallery() {
     const [images, setImages] = useState([])
+    const [n, setN]=useState(10)
+    const containerRef = useRef<HTMLDivElement>(null)
+    const { ref, entry } = useIntersection({
+        root: containerRef.current,
+        threshold: 1,
+    })
     useEffect(() => {
         (async () => {
             const resp = await fetch("/api");
@@ -13,36 +21,31 @@ export default function Gallery() {
             let i = 0;
             for (let result of results.resources) {
                 reducedResults.push({
-                    id: i,
                     height: result.height,
                     width: result.width,
                     public_id: result.public_id,
-                    src: ""
+                    src: `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${result.public_id}.webp`
                 });
                 i++;
-            }
-
-            const blurImagePromises = results.resources.map(async (image, i) => {
-                const response = await fetch(`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_50/${image.public_id}.webp`);
-                const buffer = await response.arrayBuffer();
-                const base64 = Buffer.from(buffer).toString("base64")
-                reducedResults[i].src=`data:image/jpeg;base64,${base64}?random=+${Math.random()}`
-                return `data:image/jpeg;base64,${base64}?random=`+Math.random();
-            });
-            const imagesWithBlurDataUrls = await Promise.all(blurImagePromises);
-
-            for (let i = 0; i < reducedResults.length; i++) {
-                reducedResults[i].blurDataUrl = imagesWithBlurDataUrls[i];
             }
 
             for (let i = reducedResults.length - 1; i > 0; i--) {
                 const j = Math.floor(Math.random() * (i + 1));
                 [reducedResults[i], reducedResults[j]] = [reducedResults[j], reducedResults[i]];
             }
+            for (i=0; i<reducedResults.length; i++) {
+                reducedResults[i].id=i
+            }
             setImages(reducedResults)
         })();
 
     }, [])
+
+    useEffect(()=>{
+        if (entry?.isIntersecting) {
+            setN((n)=>n+10)
+        }
+    }, [entry?.isIntersecting])
 
     if (!images || !images.length) {
         return <div className='mt-48 flex flex-col items-center'>
@@ -58,34 +61,26 @@ export default function Gallery() {
     return (
         <Masonry
             className='px-5'
-            items={images}
+            items={images.slice(0, n)}
             config={{
                 columns: [1, 2, 3],
                 gap: [15, 19, 24],
                 media: [640, 1024, 1280],
             }}
-            render={({ id, public_id, blurDataUrl, src }) => {
-                setTimeout(()=>{
-                    var image_dom=document.getElementById("img_"+id)
-                    image_dom.onload=async ()=>{
-                        console.log("Loaded")
-                        images[id].src=`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${images[id].public_id}.webp`
-                        setImages(images)
-                        image_dom.style.display=""
-                    }
-                    image_dom.src=images[id].src
-                }, 100)
+            render={({ id, public_id, blurDataUrl, src }, i) => {
                 return <img
+                    ref={(id==n-8) ? ref : null}
                     key={id}
                     id={"img_"+id}
                     alt="Picture by Digex Clan"
                     className="transform rounded-lg brightness-100 transition will-change-auto hover:brightness-[1.2]"
-                    style={{ transform: "translate3d(0, 0, 0)", width:"720px", display:"none" }}
+                    style={{ transform: "translate3d(0, 0, 0)", width:"720px" }}
                     placeholder="blur"
                     blurDataURL={blurDataUrl}
                     sizes="(max-width: 640px) 100vw, (max-width: 1280px) 50vw, (max-width: 1536px) 33vw, 25vw"
                     data-cursor-color="#ff00c3"
                     onClick={() => { window.open(`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/v1/${public_id}.jpg`) }}
+                    src={src}
                 />
             }}
         />

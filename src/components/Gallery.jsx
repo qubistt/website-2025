849ba1d76bf095e20d6c892b9d98897472a72091
aludayalu <pl46u3.ducklinz@gmail.com>
2024-05-02 23:08:30 +0530

    progress

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,9 +3,7 @@ import { useEffect, useState } from 'react';
 import { Masonry } from 'react-plock';
 
 export default function Gallery() {
-
     const [images, setImages] = useState([])
-
     useEffect(() => {
         (async () => {
             const resp = await fetch("/api");
@@ -19,15 +17,17 @@ export default function Gallery() {
                     height: result.height,
                     width: result.width,
                     public_id: result.public_id,
+                    src: ""
                 });
                 i++;
             }
 
-            const blurImagePromises = results.resources.map(async (image) => {
+            const blurImagePromises = results.resources.map(async (image, i) => {
                 const response = await fetch(`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_50/${image.public_id}.webp`);
                 const buffer = await response.arrayBuffer();
                 const base64 = Buffer.from(buffer).toString("base64")
-                return `data:image/jpeg;base64,${base64}`;
+                reducedResults[i].src=`data:image/jpeg;base64,${base64}?random=+${Math.random()}`
+                return `data:image/jpeg;base64,${base64}?random=`+Math.random();
             });
             const imagesWithBlurDataUrls = await Promise.all(blurImagePromises);
 
@@ -64,20 +64,30 @@ export default function Gallery() {
                 gap: [15, 19, 24],
                 media: [640, 1024, 1280],
             }}
-            render={({ id, public_id, blurDataUrl }) => (
-                <img
+            render={({ id, public_id, blurDataUrl, src }) => {
+                setTimeout(()=>{
+                    var image_dom=document.getElementById("img_"+id)
+                    image_dom.onload=async ()=>{
+                        console.log("Loaded")
+                        images[id].src=`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${images[id].public_id}.webp`
+                        setImages(images)
+                        image_dom.style.display=""
+                    }
+                    image_dom.src=images[id].src
+                }, 100)
+                return <img
                     key={id}
+                    id={"img_"+id}
                     alt="Picture by Digex Clan"
                     className="transform rounded-lg brightness-100 transition will-change-auto hover:brightness-[1.2]"
-                    style={{ transform: "translate3d(0, 0, 0)", width:"720px" }}
+                    style={{ transform: "translate3d(0, 0, 0)", width:"720px", display:"none" }}
                     placeholder="blur"
                     blurDataURL={blurDataUrl}
-                    src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.webp`}
                     sizes="(max-width: 640px) 100vw, (max-width: 1280px) 50vw, (max-width: 1536px) 33vw, 25vw"
                     data-cursor-color="#ff00c3"
                     onClick={() => { window.open(`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/v1/${public_id}.jpg`) }}
                 />
-            )}
+            }}
         />
     )
 }

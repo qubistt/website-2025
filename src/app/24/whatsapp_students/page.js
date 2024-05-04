6bde367976235f3a894852f7f310112c09038ddc
Atharv varshney <Atharvvarshney07@gmail.com>
2024-05-04 19:02:41 +0530

    removing 24 folder

--- src/app/24/whatsapp_students/page.js
+++
@@ -1,4 +0,0 @@
-import { redirect } from 'next/navigation'
-export default function whatsapp_students() {
-    redirect("https://chat.whatsapp.com/D911Rl2lCFR8vebTlylwVX")
-}
--- src/app/24/whatsapp_teachers/page.js
+++
@@ -1,4 +0,0 @@
-import { redirect } from 'next/navigation'
-export default function whatsapp_teachers() {
-    redirect("https://chat.whatsapp.com/H25qMnRfWpjIL0Dm8uQBfH")
-}

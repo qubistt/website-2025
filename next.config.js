f779f0e005cd941fc47ce21833a6d71640c8ca22
Atharv varshney <Atharvvarshney07@gmail.com>
2024-05-04 18:40:51 +0530

    adding shortner links

--- next.config.js
+++ next.config.js
@@ -3,6 +3,30 @@ module.exports = {
     images: {
         domains: ["images.pexels.com", "res.cloudinary.com"],
     },
+    async redirects() {
+        return [
+            {
+                source: '/24/reg',
+                destination: 'https://docs.google.com/forms/d/1j4POdIVxZ2dZmTF2KxIeR3rJsS8SGsTV5TI0RuS2SQU/viewform?edit_requested=true',
+                permanent: true,
+            },
+            {
+                source: '/24/discord',
+                destination: 'https://discord.gg/Rkm6tJEwV2',
+                permanent: true,
+            },
+            {
+                source: '/24/whatsapp_teachers',
+                destination: 'https://chat.whatsapp.com/H25qMnRfWpjIL0Dm8uQBfH',
+                permanent: true,
+            },
+            {
+                source: '/24/whatsapp_students',
+                destination: 'https://chat.whatsapp.com/D911Rl2lCFR8vebTlylwVX',
+                permanent: true,
+            },
+        ]
+    },
     webpack: (config, { isServer }) => {
         // If client-side, don't polyfill `fs`
         if (!isServer) {
